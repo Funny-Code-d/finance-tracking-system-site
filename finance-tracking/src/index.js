102b63017e49dc4584d1f7129bbe9f0c89780c6c
Funny-Code-d <sosnin_dienis@mail.ru>
2022-05-03 14:01:43 +0700

    Первые шаги в react

---
+++ finance-tracking/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/index.css'
+import NuvButtons from './buttons_description/nav_button';
+import Header from './header/header';
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<NuvButtons />)
+
+const header = ReactDOM.createRoot(document.getElementById("header"))
+header.render(<Header />)
+
